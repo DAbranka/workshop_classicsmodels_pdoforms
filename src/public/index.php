6dbb7af7881b0ba92d15d6faf9923091e364ccae
Abranka <davidabranka@gmail.com>
2022-12-13 15:50:47 +0100

    index.php updated

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,10 @@
 <?php
 declare(strict_types=1);
 
-echo 'It works !';
+include "./oop-exercices/exercice_1_classes.php";
+include "./oop-exercices/exercice_2_extending.php";
+include "./oop-exercices/exercice_3_private.php";
+include "./oop-exercices/exercice_4_protected.php";
+include "./oop-exercices/exercice_5_public.php";
+include "./oop-exercices/exercice_6_const.php";
+include "./oop-exercices/exercice_7_static.php";
